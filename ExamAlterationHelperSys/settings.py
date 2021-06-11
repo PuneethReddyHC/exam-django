0ac5313569716035a9bade11d11e2ea6cafefcaa
puneethreddyhc <puneethreddy951@gmail.com>
2021-06-11 08:18:08 +0530

    database commit

--- ExamAlterationHelperSys/settings.py
+++ ExamAlterationHelperSys/settings.py
@@ -117,9 +117,25 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'examproject',
+
+        'HOST': 'django.ciejprzkee6v.ap-south-1.rds.amazonaws.com',
+
+        'PORT': 5432,
+
     }
 }
 
